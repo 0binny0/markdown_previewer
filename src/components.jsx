de465d36d4575d6d417b6fe7cf9fc750eb82102e
0binny0 <binnyhh@protonmail.com>
2024-05-05 11:57:58 -0700

    included HTML textarea in Editor

---
+++ src/components.jsx
@@ -0,0 +1,22 @@
+
+function Previewer() {
+
+}
+
+function Editor() {
+    return <>
+        <form>
+            <textarea id="editor" className="box"></textarea>
+        </form>
+    </>
+}
+
+function Window() {
+
+}
+
+function MarkdownPreviewer() {
+
+}
+
+export {MarkdownPreviewer, Editor, Previewer};
--- src/components.test.jsx
+++ src/components.test.jsx
@@ -1,10 +1,30 @@
 
 import {test, expect} from "vitest";
+import {render} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import {initialize_markdown} from "./helpers.js";
+import {MarkdownPreviewer, Editor, Previewer} from "./components.jsx";
 
 test("Verify that markdown is set to git flavored markdown", () => {
     const markdown = initialize_markdown();
     expect(markdown.defaults.breaks).toBe(true);
     expect(markdown.defaults.gfm).toBe(true);
 });
+
+test("Verify that a user can edit markdown", () => {
+    const {queryByRole} = render(<Editor />);
+    const textarea = queryByRole("textbox");
+    expect(textarea).toBeInTheDocument();
+});
+
+// test("Verify that markdown is converted to HTML", () => {
+//     const main_element = document.createElement("main");
+//     const {getByRole} = render(
+//         <MarkdownPreviewer />, {container: document.body.appendChild(main_element)}
+//     );
+//     userEvent.type(getByRole("textbox", "# hello world"));
+//     const html_previewer = getByRole("region", {"name": "Converted HTML"});
+//     expect(html_previewer).toContainElement("<h1>hello world</h1>");
+//
+// });
