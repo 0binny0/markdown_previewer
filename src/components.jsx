4f59391a0cd06fb1e3fea476816fa02203476312
0binny0 <binnyhh@protonmail.com>
2024-05-06 10:57:18 -0700

    tested Window component

--- src/components.jsx
+++ src/components.jsx
@@ -1,24 +1,51 @@
 
-function Previewer() {
-    return <>
-        <section aria-label="markdown converted to HTML" className="box"></section>
-    </>
-}
+import {useRef, forwardRef} from "react";
 
-function Editor() {
+import {markdown_converter} from './helpers.js';
+
+const Previewer = forwardRef(function Previewer(props, ref) {
+    return <>
+        <output for="editor" contentEditable="true" ref={ref} className="box"></output>
+    </>
+});
+
+function Editor(props) {
     return <>
         <form>
-            <textarea id="editor" className="box"></textarea>
+            <textarea id="editor" className="box" onChange={props.onTyping}></textarea>
         </form>
     </>
 }
 
-function Window() {
+function Window({children, name}) {
+    return <section className="window_wrapper">
+        <div className="heading_wrapper">
+            <h2>{name}</h2>
+            {/* icon widget goes here */}
+        </div>
+        {children}
+    </section>
 
 }
 
 function MarkdownPreviewer() {
 
+    const html_render_ref = useRef(null);
+
+    function handleMarkdownInput(e) {
+        html_render_ref.current.innerHTML = markdown_converter.parse(
+            e.currentTarget.value
+        );
+    }
+
+    return <main className="main_content">
+        <Window name={"Previewer"}>
+            <Previewer ref={html_render_ref} />
+        </Window>
+        <Window name={"Editor"}>
+            <Editor onTyping={handleMarkdownInput} />
+        </Window>
+    </main>
 }
 
-export {MarkdownPreviewer, Editor, Previewer};
+export {MarkdownPreviewer, Editor, Previewer, Window};
--- src/components.test.jsx
+++ src/components.test.jsx
@@ -3,13 +3,12 @@ import {test, expect} from "vitest";
 import {render} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-import {initialize_markdown} from "./helpers.js";
-import {MarkdownPreviewer, Editor, Previewer} from "./components.jsx";
+import {markdown_converter} from "./helpers.js";
+import {MarkdownPreviewer, Editor, Previewer, Window} from "./components.jsx";
 
 test("Verify that markdown is set to git flavored markdown", () => {
-    const markdown = initialize_markdown();
-    expect(markdown.defaults.breaks).toBe(true);
-    expect(markdown.defaults.gfm).toBe(true);
+    expect(markdown_converter.defaults.breaks).toBe(true);
+    expect(markdown_converter.defaults.gfm).toBe(true);
 });
 
 test("Verify that a user can edit markdown", () => {
@@ -20,7 +19,7 @@ test("Verify that a user can edit markdown", () => {
 
 test("Verify that a section of the page is reserved to view rendered HTML", () => {
     const {getByRole} = render(<Previewer />);
-    const html_previewer = getByRole("region", {"name": "markdown converted to HTML"});
+    const html_previewer = getByRole("status");
     expect(html_previewer).toBeInTheDocument();
 });
 
@@ -29,8 +28,18 @@ test("Verify that a section of the page is reserved to view rendered HTML", () =
 //     const {getByRole} = render(
 //         <MarkdownPreviewer />, {container: document.body.appendChild(main_element)}
 //     );
-//     userEvent.type(getByRole("textbox", "# hello world"));
-//     const html_previewer = getByRole("region", {"name": "Converted HTML"});
-//     expect(html_previewer).toContainElement("<h1>hello world</h1>");
-//
+//     userEvent.type(getByRole("textbox"), "# hello world");
+//     const html_previewer = getByRole("region", {"name": "markdown converted to HTML"});
+//     // expect(html_previewer).toHaveTextContent("hello world");
 // });
+
+test("Verify that a heading is displayed for a Window", () => {
+    const main_element = document.createElement("main");
+    const {getByText, getByRole} = render(
+        <Window name="Editor"><Editor /></Window>, {container: document.body.appendChild(main_element)}
+    );
+    const window_heading = getByRole("heading", {"level": 2});
+    expect(window_heading).toHaveTextContent("Editor");
+    const markdown_editor = getByRole("textbox");
+    expect(markdown_editor).toHaveAttribute("id", "editor");
+});
